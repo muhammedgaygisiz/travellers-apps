ae900f1aa7cee72f25e16dd9b79c22496e35bb61
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-05-20 14:45:23 +0200

    Integrate Ionic

--- apps/prices/src/app/app.module.ts
+++ apps/prices/src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {
+  PreloadAllModules,
+  RouteReuseStrategy,
+  RouterModule,
+  Routes,
+} from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

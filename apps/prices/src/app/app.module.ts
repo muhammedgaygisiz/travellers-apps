923594c0e76bfd36237a1c003407996c7811e0b8
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-06-16 18:36:16 +0200

    Introduce lib and module

--- apps/prices/src/app/app.module.ts
+++ apps/prices/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import {NgModule} from '@angular/core';
-import {IonicModule} from '@ionic/angular';
-import {PricesShellModule} from '@travellers-apps/prices/shell/feature';
-import {AppComponent} from './app.component';
-import {FooterToolBarFeatureModule} from "@travellers-apps/prices/footer-tool-bar/feature";
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { PricesShellModule } from '@travellers-apps/prices/shell/feature';
+import { AppComponent } from './app.component';
+import { FooterToolBarFeatureModule } from '@travellers-apps/prices/footer-tool-bar/feature';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [IonicModule.forRoot(), PricesShellModule, FooterToolBarFeatureModule],
+  imports: [
+    IonicModule.forRoot(),
+    PricesShellModule,
+    FooterToolBarFeatureModule,
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ libs/prices/header-tool-bar/feature/jest.config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+export default {
+  displayName: 'prices-header-tool-bar-feature',
+  preset: '../../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../../../coverage/libs/prices/header-tool-bar/feature',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};

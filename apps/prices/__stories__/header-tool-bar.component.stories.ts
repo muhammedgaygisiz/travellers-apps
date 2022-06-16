1fa8fc6fba02e7b444b2e8f69b91c1f2b7e755d8
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-06-16 20:48:16 +0200

    Cleanup

--- apps/prices/__stories__/header-tool-bar.component.stories.ts
+++ apps/prices/__stories__/header-tool-bar.component.stories.ts
@@ -1,11 +1,5 @@
 import {IonicModule} from '@ionic/angular';
 import {Meta, moduleMetadata, Story} from '@storybook/angular';
-import {
-  HomeComponent,
-  HomeComponentModule,
-} from '@travellers-apps/prices/home/feature';
-import {FooterToolBarComponent} from "../../../libs/prices/footer-tool-bar/feature/src/lib/footer-tool-bar.component";
-import {FooterToolBarFeatureModule} from "@travellers-apps/prices/footer-tool-bar/feature";
 import {HeaderToolBarComponent} from "../../../libs/prices/header-tool-bar/feature/src/lib/header-tool-bar.component";
 import {HeaderToolBarFeatureModule} from "@travellers-apps/prices/header-tool-bar/feature";
 

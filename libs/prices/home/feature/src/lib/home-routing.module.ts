97053ef15015c0d6db5dfea725a522dbf2fa6af2
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-03 19:13:47 +0200

    Introduce barrel for integration

--- libs/prices/home/feature/src/lib/home-routing.module.ts
+++ libs/prices/home/feature/src/lib/home-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeContainer} from "./integration/home.container";
+import {HomeContainer} from "./integration";
 
 const routes: Routes = [
   {

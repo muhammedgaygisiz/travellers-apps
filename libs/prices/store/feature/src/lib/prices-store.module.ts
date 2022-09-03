001b9780e0eeb91b84b7942590bdf3c1e50dbe88
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-03 22:48:15 +0200

    Cleanup

--- libs/prices/store/feature/src/lib/prices-store.module.ts
+++ libs/prices/store/feature/src/lib/prices-store.module.ts
@@ -1,4 +1,4 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {StoreModule} from "@ngrx/store";
 import {EffectsModule} from "@ngrx/effects";

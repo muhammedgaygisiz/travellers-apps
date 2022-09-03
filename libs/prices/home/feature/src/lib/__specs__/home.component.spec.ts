401769731f3fbad9d98c1a159c5d44a3c70c9a73
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-03 19:12:47 +0200

    Introduce barrel for components

--- libs/prices/home/feature/src/lib/__specs__/home.component.spec.ts
+++ libs/prices/home/feature/src/lib/__specs__/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from '../components/home.component';
+import { HomeComponent } from '../components';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;

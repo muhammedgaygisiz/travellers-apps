4b651e5df09136e51f97fcdf0770d688cce9f894
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-18 19:31:49 +0200

    Remove dummy change

--- libs/prices/card/feature/src/lib/card.component.ts
+++ libs/prices/card/feature/src/lib/card.component.ts
@@ -1,8 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MostSearchedItem } from '@travellers-apps/prices/store/feature';
 
-console.log('Some log!');
-
 @Component({
   selector: 'ta-card',
   templateUrl: './card.component.html',

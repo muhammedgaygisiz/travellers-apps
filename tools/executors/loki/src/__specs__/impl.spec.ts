5e2bc988d676b5495fb72902ad63090881620b22
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-23 10:22:06 +0200

    Add initial test

--- tools/executors/loki/src/__specs__/impl.spec.ts
+++ tools/executors/loki/src/__specs__/impl.spec.ts
@@ -1,3 +1,15 @@
 describe('loki nx executor', () => {
-  describe('given the project.json file of the prices app', () => {});
+  describe('given the project.json file of the prices app', () => {
+    it('should execute the command with right parameters', () => {
+      const expected =
+        'loki ' +
+        '--requireReferernce ' +
+        '--reactUri file:/Users/muhammedgaygisiz/DEV/travellers-apps/dist/storybook/prices ' +
+        '--reference=../../apps/prices/.loki/reference ' +
+        '--difference=../../apps/prices/.loki/difference ' +
+        '--output=../../apps/prices/.loki/current';
+
+      expect(result).toBe(expected);
+    });
+  });
 });

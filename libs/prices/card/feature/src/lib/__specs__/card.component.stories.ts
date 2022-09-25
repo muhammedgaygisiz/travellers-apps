d4ce17edb89e1dc00d90847181db073c8577a859
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-25 20:36:00 +0200

    Extract content from card component

--- libs/prices/card/feature/src/lib/__specs__/card.component.stories.ts
+++ libs/prices/card/feature/src/lib/__specs__/card.component.stories.ts
@@ -1,24 +1,50 @@
-import {CardComponent} from "../card.component";
-import {Meta, moduleMetadata, Story} from "@storybook/angular";
-import {IonicModule} from "@ionic/angular";
-import {CardFeatureModule} from "@travellers-apps/prices/card/feature";
-import {getIonicConfig} from "@travellers-apps/utils-common";
+import { CardComponent } from '../card.component';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { IonicModule } from '@ionic/angular';
+import { CardFeatureModule } from '../card-feature.module';
+import { getIonicConfig } from '@travellers-apps/utils-common';
 
 export default {
   title: 'Components/Card',
   component: CardComponent,
   decorators: [
     moduleMetadata({
-      imports: [
-        IonicModule.forRoot(getIonicConfig()),
-        CardFeatureModule
-      ]
-    })
-  ]
+      imports: [IonicModule.forRoot(getIonicConfig()), CardFeatureModule],
+    }),
+  ],
 } as Meta<CardComponent>;
+
+const EmptyTemplate: Story<CardComponent> = (args: CardComponent) => ({
+  props: args,
+  template: `
+    <ta-card>
+        <div class="ion-margin">
+          Cart Content has to take care of its own margin
+        </div>
+    </ta-card>`,
+});
+
+export const Empty = EmptyTemplate.bind({});
+Empty.args = {};
 
 const Template: Story<CardComponent> = (args: CardComponent) => ({
   props: args,
+  template: `
+    <ta-card>
+      <ion-card-header>
+        <ion-card-subtitle>MOST SEARCHED IN</ion-card-subtitle>
+        <ion-card-title>Marrakech</ion-card-title>
+      </ion-card-header>
+
+      <ion-card-content>
+        <div>
+          <img [src]="entry?.src"/>
+          <h2>{{entry?.name}}</h2>
+          <h1>{{entry?.price | currency}}</h1>
+        </div>
+      </ion-card-content>
+    </ta-card>
+  `,
 });
 
 export const Primary = Template.bind({});
@@ -28,5 +54,5 @@ Primary.args = {
     src: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Autumn_Red_peaches.jpg',
     name: 'Peaches',
     price: 0.2,
-  }
+  },
 };

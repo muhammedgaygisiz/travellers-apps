ba558ad8a41c1f59c15b547ae520c4c5d7687375
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-25 22:18:06 +0200

    Fix tests and add test to build pipeline

--- apps/prices/src/app/__specs__/app.component.spec.ts
+++ apps/prices/src/app/__specs__/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from '../app.component';
+import { IonicModule } from '@ionic/angular';
+import { getIonicConfig } from '@travellers-apps/utils-common';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(getIonicConfig()), RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -18,14 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('prices');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome prices'
-    );
   });
 });

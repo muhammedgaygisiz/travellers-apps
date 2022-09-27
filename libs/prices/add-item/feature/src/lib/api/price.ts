489f2282e68de7b4197df99f1a9209406b6309dd
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-27 07:22:37 +0200

    Add field for image src

--- libs/prices/add-item/feature/src/lib/api/price.ts
+++ libs/prices/add-item/feature/src/lib/api/price.ts
@@ -1,7 +1,8 @@
-import {FormControl} from "@angular/forms";
+import { FormControl } from '@angular/forms';
 
 export interface Price {
   productName: FormControl<string | null>;
   price: FormControl<number | null>;
+  src: FormControl<string | null>;
   location: FormControl<string | null>;
 }

f1573173428d70207cbe1a3bcfaa259d05f84f8d
Muhammed Gaygisiz <54054345+muhammedgaygisiz@users.noreply.github.com>
2022-09-30 15:33:30 +0200

    #68: introduction and introducing capacitor (#69)
    * Add capacitor dependencies
    
    * Initialize capacitor
    
    * Add capacitor ios and android dependencies
    
    * Add Android and iOS native applications
    
    * Adjust capacitor.config.ts for webDir option
    
    * Add capacitor run scripts
    
    * Add cap sync to pipeline

---
+++ android/app/src/main/java/com/travellersapps/prices/MainActivity.java
@@ -0,0 +1,5 @@
+package com.travellersapps.prices;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.travellersapps.prices',
+  appName: 'prices',
+  webDir: 'dist/apps/prices',
+  bundledWebRuntime: false,
+};
+
+export default config;

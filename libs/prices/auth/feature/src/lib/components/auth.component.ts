cc9573a9b9d0d72a2070bc1bd3905c8f1f137f88
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-30 16:23:12 +0200

    Add component and adjust file system

---
+++ libs/prices/auth/feature/src/lib/components/auth.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'travellers-apps-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css'],
+})
+export class AuthComponent {}
--- libs/prices/auth/feature/src/lib/prices-auth-feature.module.ts
+++ libs/prices/auth/feature/src/lib/prices-auth-feature.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthComponent } from './components/auth.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [AuthComponent],
 })
 export class PricesAuthFeatureModule {}

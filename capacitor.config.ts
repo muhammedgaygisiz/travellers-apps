e92405bc4ccbbde4479ce062e70f885723a18146
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-30 15:05:59 +0200

    Initialize capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.travellersapps.prices',
+  appName: 'prices',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;

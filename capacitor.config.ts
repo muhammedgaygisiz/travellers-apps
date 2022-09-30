397dca4e573f147002dd92a8d5cb13422a30573e
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-09-30 15:11:28 +0200

    Adjust capacitor.config.ts for webDir option

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.travellersapps.prices',
   appName: 'prices',
-  webDir: 'www',
+  webDir: 'dist/apps/prices',
   bundledWebRuntime: false,
 };
 

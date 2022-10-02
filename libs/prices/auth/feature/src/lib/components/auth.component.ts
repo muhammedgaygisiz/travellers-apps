40b56eeb4b6ff2caf0a44ed3037a43617b8f85b2
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-10-02 10:24:32 +0200

    Implement validation of password

--- libs/prices/auth/feature/src/lib/components/auth.component.ts
+++ libs/prices/auth/feature/src/lib/components/auth.component.ts
@@ -1,38 +1,79 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthCredentials } from '../api/auth-credentials';
-import { tap } from 'rxjs';
+import { map, shareReplay, tap } from 'rxjs';
+
+const lowerCase = /[a-z]/;
+const upperCase = /[A-Z]/;
+const digit = /[0-9]/;
+const minLength = 8;
 
 @Component({
   selector: 'ta-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
   @Output()
   public submitAuth: EventEmitter<AuthCredentials> = new EventEmitter();
 
   public authFormGroup: FormGroup = new FormGroup<AuthCredentials>({
     username: new FormControl<string>('', [Validators.required]),
-    password: new FormControl<string>('', [Validators.required]),
+    password: new FormControl<string>('', this.getPasswordValidator()),
   });
 
-  public hasALowerCaseLetter = false;
-  public hasAnUpperCaseLetter = false;
-  public hasANumber = false;
-  public is8CaracterLong = false;
+  private password$ = this.authFormGroup.controls['password'].valueChanges.pipe(
+    shareReplay(),
+    tap((password) => console.log('#mo', password))
+  );
 
-  ngOnInit(): void {
-    this.authFormGroup.controls['password'].valueChanges
-      .pipe(
-        tap((password) => {
-          this.hasALowerCaseLetter = true;
-          this.hasAnUpperCaseLetter = true;
-          this.hasANumber = true;
-          this.is8CaracterLong = password.length() > 8;
-          console.log(password);
-        })
-      )
-      .subscribe();
+  public hasLowerCaseLetter$ = this.password$.pipe(
+    map((password) => this.test(password).forPattern(lowerCase))
+  );
+
+  public hasUpperCaseLetter$ = this.password$.pipe(
+    map((password) => this.test(password).forPattern(upperCase))
+  );
+
+  public hasNumber$ = this.password$.pipe(
+    map((password) => this.test(password).forPattern(digit))
+  );
+
+  public hasLength8 = this.password$.pipe(
+    map((password) => this.test(password).forLength(minLength))
+  );
+
+  private test(password: string) {
+    return {
+      forPattern: (pattern: RegExp) => {
+        return this.calculateColor(password, () => pattern.test(password));
+      },
+      forLength: (length: number) => {
+        return this.calculateColor(password, () => password.length >= length);
+      },
+    };
+  }
+
+  calculateColor(password: string, condition: () => boolean) {
+    if (!password) {
+      return '';
+    }
+
+    const conditionFulfilled = condition();
+    if (conditionFulfilled) {
+      return 'success';
+    }
+
+    return 'danger';
+  }
+
+  private getPasswordValidator() {
+    return Validators.compose([
+      Validators.required,
+      Validators.pattern(lowerCase),
+      Validators.pattern(upperCase),
+      Validators.pattern(digit),
+      Validators.minLength(minLength),
+    ]);
   }
 }

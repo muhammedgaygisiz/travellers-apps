5e0623e98f59683b04e9b4129ec1b85f565fcf80
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-10-09 11:35:56 +0200

    Fix storybook

--- libs/prices/home/feature/src/lib/__specs__/home.component.stories.ts
+++ libs/prices/home/feature/src/lib/__specs__/home.component.stories.ts
@@ -28,18 +28,21 @@ Primary.args = {
       src: 'https://upload.wikimedia.org/wikipedia/commons/6/69/Aroma_%28apple%29.jpg',
       name: 'Apples',
       price: 0.2,
+      location: 'Marrakech',
     },
     {
       id: '2',
       src: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Assortment_of_pears.jpg',
       name: 'Bears',
       price: 0.2,
+      location: 'Marrakech',
     },
     {
       id: '3',
       src: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Autumn_Red_peaches.jpg',
       name: 'Peaches',
       price: 0.2,
+      location: 'Marrakech',
     },
   ],
 };

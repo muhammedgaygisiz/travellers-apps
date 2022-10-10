22538025065f5e5d91119a61edc9597dbedc1eb6
Muhammed Gaygisiz <54054345+muhammedgaygisiz@users.noreply.github.com>
2022-10-10 08:31:37 +0200

    #94: Prevent making request in unauthorized state (#97)
    * Prevent making request in unauthorized state - if user ever gets into the page
    
    * Surpress eslint error
    
    * Cleanup
    
    * Cleanup

--- libs/prices/store/feature/src/lib/mostSearched/effects.ts
+++ libs/prices/store/feature/src/lib/mostSearched/effects.ts
@@ -1,13 +1,27 @@
+/* eslint-disable no-unused-vars */
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { itemsLoaded, loadItems, saveItem } from './actions';
-import { catchError, EMPTY, map, mergeMap, switchMap, tap } from 'rxjs';
+import {
+  catchError,
+  EMPTY,
+  filter,
+  map,
+  mergeMap,
+  switchMap,
+  tap,
+  withLatestFrom,
+} from 'rxjs';
 import { MostSearchedService } from '@travellers-apps/prices/firestore/feature';
 import { MostSearchedItem } from '@travellers-apps/utils-common';
-import { NavController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
+import { fromAuth } from '../auth/index';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class MostSearchedItemsEffects {
+  isAuthenticated$ = this.store.select(fromAuth.selectIsAuthenticated);
+
   loadMostSearchedItemsEffect$ = createEffect(() =>
     this.actions$.pipe(
       ofType(loadItems.type),
@@ -26,13 +40,32 @@ export class MostSearchedItemsEffects {
     () =>
       this.actions$.pipe(
         ofType(saveItem.type),
-        switchMap(({ item }) =>
+        withLatestFrom(this.isAuthenticated$),
+        tap(
+          async ([_, isAuthenticated]) =>
+            await this.showUnauthorizedToast(isAuthenticated)
+        ),
+        filter(([_, isAuthenticated]) => isAuthenticated),
+        switchMap(([{ item }]) =>
           this.mostSearchedService.saveMostSearchedItem$(item)
         ),
-        tap(() => this.navController.back())
+        tap(() => this.navController.back()),
+        catchError(() => EMPTY)
       ),
     { dispatch: false }
   );
+
+  private async showUnauthorizedToast(isAuthenticated: boolean) {
+    if (!isAuthenticated) {
+      const toast = await this.toastController.create({
+        message: 'You have to be logged in',
+        duration: 1500,
+        position: 'bottom',
+      });
+
+      await toast.present();
+    }
+  }
 
   constructor(
     // eslint-disable-next-line no-unused-vars
@@ -40,7 +73,11 @@ export class MostSearchedItemsEffects {
     // eslint-disable-next-line no-unused-vars
     private readonly mostSearchedService: MostSearchedService,
     // eslint-disable-next-line no-unused-vars
-    private readonly navController: NavController
+    private readonly navController: NavController,
+    // eslint-disable-next-line no-unused-vars
+    private readonly store: Store,
+    // eslint-disable-next-line no-unused-vars
+    private toastController: ToastController
   ) {}
 
   private toItemsLoadedAction(mostSearchedEntries: MostSearchedItem[]) {

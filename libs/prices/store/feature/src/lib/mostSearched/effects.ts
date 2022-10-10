d78ea615880edf0b5f3d508247b8bc15a374fe9d
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-10-10 08:23:09 +0200

    Cleanup

--- libs/prices/store/feature/src/lib/mostSearched/effects.ts
+++ libs/prices/store/feature/src/lib/mostSearched/effects.ts
@@ -18,8 +18,6 @@ import { NavController, ToastController } from '@ionic/angular';
 import { fromAuth } from '../auth/index';
 import { Store } from '@ngrx/store';
 
-// This is some dummy change
-
 @Injectable()
 export class MostSearchedItemsEffects {
   isAuthenticated$ = this.store.select(fromAuth.selectIsAuthenticated);

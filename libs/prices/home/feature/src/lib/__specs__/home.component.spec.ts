842a2521a190f0ac4d0367d2c027e860521224a4
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-10-15 15:16:56 +0200

    Fix unit tests

--- libs/prices/home/feature/src/lib/__specs__/home.component.spec.ts
+++ libs/prices/home/feature/src/lib/__specs__/home.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from '../components';
-import { HomeComponentModule } from '../home.module';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,7 +8,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponentModule],
+      imports: [HomeComponent],
     }).compileComponents();
   });
 

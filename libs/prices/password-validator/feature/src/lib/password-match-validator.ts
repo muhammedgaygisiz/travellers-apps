d33689dd161698f41978b6f6a116584669af84bf
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-10-25 11:56:33 +0200

    Cleanup

--- libs/prices/password-validator/feature/src/lib/password-match-validator.ts
+++ libs/prices/password-validator/feature/src/lib/password-match-validator.ts
@@ -2,11 +2,9 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 const passwordMatchValidator = (
   control: AbstractControl
-): ValidationErrors | null => {
-  return control.get('password')?.value ===
-    control.get('passwordConfirm')?.value
+): ValidationErrors | null =>
+  control.get('password')?.value === control.get('passwordConfirm')?.value
     ? null
     : { mismatch: true };
-};
 
 export default passwordMatchValidator;

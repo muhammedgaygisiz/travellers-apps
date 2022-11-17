109c0b2ff4d746f934cc7184c3cd9ddc017a4c78
Muhammed Gaygisiz <muhammed.gaygisiz@sbb.ch>
2022-11-17 19:02:15 +0100

    chore: cleanup

--- libs/prices/auth/feature/src/lib/__specs__/auth.service.spec.ts
+++ libs/prices/auth/feature/src/lib/__specs__/auth.service.spec.ts
@@ -3,17 +3,10 @@ import { AuthService } from '../integration/auth.service';
 import { TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 
-// const assertDeepEqual = (actual: any, expected: any) => {
-//   expect(actual).toEqual(expected);
-// };
-
 describe('AuthService', () => {
-  // let scheduler: TestScheduler;
   let service: AuthService;
 
   beforeEach(async () => {
-    // scheduler = new TestScheduler(assertDeepEqual);
-
     const initialState = {};
     await TestBed.configureTestingModule({
       providers: [provideMockStore({ initialState })],

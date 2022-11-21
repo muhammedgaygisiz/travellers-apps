232e178896d5ca2e61308fc78ed4d29fcc8f0b21
Muhammed Gaygisiz <muhammed.gaygisiz@sbb.ch>
2022-11-21 09:43:09 +0100

    feat: fix rendering of data structure

--- libs/kosaml/site/feature/src/lib/components/sidebar/project/project.component.ts
+++ libs/kosaml/site/feature/src/lib/components/sidebar/project/project.component.ts
@@ -5,13 +5,7 @@ import {
   MatTreeFlattener,
 } from '@angular/material/tree';
 import { FileNode } from '@travellers-apps/kosaml/store/feature';
-import { DataSource } from '@angular/cdk/collections';
-import { Observable } from 'rxjs';
 
-/**
- * Flattened tree node that has been created from a FileNode through the flattener. Flattened
- * nodes include level index and whether they can be expanded or not.
- */
 export interface FlatTreeNode {
   name: string;
   type: string;
@@ -25,71 +19,38 @@ export interface FlatTreeNode {
   styleUrls: ['./project.component.scss'],
 })
 export class ProjectComponent implements OnChanges {
-  /** The TreeControl controls the expand/collapse state of tree nodes.  */
-  treeControl: FlatTreeControl<FlatTreeNode> = new FlatTreeControl(
-    this.getLevel,
-    this.isExpandable
-  );
-
-  /** The TreeFlattener is used to generate the flat list of items from hierarchical data. */
-  treeFlattener: MatTreeFlattener<FileNode, FlatTreeNode> =
-    new MatTreeFlattener<FileNode, FlatTreeNode>(
-      this.transformer,
-      this.getLevel,
-      this.isExpandable,
-      this.getChildren
-    );
-
-  /** The MatTreeFlatDataSource connects the control and flattener to provide data. */
-  dataSource:
-    | DataSource<FlatTreeNode>
-    | FlatTreeNode[]
-    | Observable<FlatTreeNode[]> = new MatTreeFlatDataSource(
-    this.treeControl,
-    this.treeFlattener
-  );
-
   @Input()
   project: FileNode[] | null = [];
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['project']) {
-      const data = changes['project'].currentValue;
-      this.dataSource = data;
-      this.treeControl.dataNodes = data;
+  treeControl = new FlatTreeControl<FlatTreeNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
 
-      this.treeControl.expandAll();
-    }
-  }
-
-  /** Transform the data to something the tree can read. */
-  transformer(node: FileNode, level: number) {
-    return {
+  treeFlattener = new MatTreeFlattener<FileNode, FlatTreeNode>(
+    (node: FileNode, level: number) => ({
       level,
       name: node.name,
       type: node.type,
       expandable: !!node.children,
       link: node.link,
-    };
+    }),
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['project']) {
+      this.dataSource.data = changes['project'].currentValue;
+    }
   }
 
-  /** Get the level of the node */
-  getLevel(node: FlatTreeNode) {
-    return node.level;
+  constructor() {
+    this.dataSource.data = this.project ?? [];
   }
 
-  /** Get whether the node is expanded or not. */
-  isExpandable(node: FlatTreeNode) {
-    return node.expandable;
-  }
-
-  /** Get whether the node has children or not. */
-  hasChild(index: number, node: FileNode) {
-    return !!node.children;
-  }
-
-  /** Get the children for the node. */
-  getChildren(node: FileNode) {
-    return node.children;
-  }
+  hasChild = (_: number, node: FlatTreeNode) => node.expandable;
 }

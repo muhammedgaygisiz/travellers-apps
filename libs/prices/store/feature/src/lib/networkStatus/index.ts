40fb0ddcdc1b70c18512825bd25c34de3c664954
Muhammed Gaygisiz <muhammed.gaygisiz@sbb.ch>
2022-12-10 17:19:50 +0100

    chore: add selector for isOnline

--- libs/prices/store/feature/src/lib/networkStatus/index.ts
+++ libs/prices/store/feature/src/lib/networkStatus/index.ts
@@ -1,9 +1,11 @@
 import { key } from './key';
 import { reducer } from './reducer';
+import { selectIsOnline } from './selectors';
 
 const fromNetworkStatus = {
   key,
   reducer,
+  selectIsOnline,
 };
 
 export { fromNetworkStatus };

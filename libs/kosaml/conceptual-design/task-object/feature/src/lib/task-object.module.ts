a4b2b339249a393e916a367ea549df3b6ae0a281
Muhammed Gaygisiz <muhammed.gaygisiz@sbb.ch>
2023-01-14 20:39:43 +0100

    chore: execute migration

--- libs/kosaml/conceptual-design/task-object/feature/src/lib/task-object.module.ts
+++ libs/kosaml/conceptual-design/task-object/feature/src/lib/task-object.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TaskObjectRoutingModule } from './task-object-routing.module';
 import { TaskObjectPageComponent } from './containers/task-object-page.component';
 import { KosamlPageFeatureModule } from '@travellers-apps/kosaml/page/feature';
-import { MatTableModule } from '@angular/material/table';
+import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
 import { TaskObjectComponent } from './components/task-object/task-object.component';
 
 @NgModule({

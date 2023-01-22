929eb99b533f9fc3cb442cea3113bf9451df56e7
Muhammed Gaygisiz <muhammed.gaygisiz@sbb.ch>
2023-01-22 18:09:25 +0100

    test: fix unit tests for header and app components

--- apps/kosaml/src/app/__specs__/app.component.spec.ts
+++ apps/kosaml/src/app/__specs__/app.component.spec.ts
@@ -1,13 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from '../app.component';
 import { provideMockStore } from '@ngrx/store/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
       providers: [provideMockStore()],
     }).compileComponents();
   });

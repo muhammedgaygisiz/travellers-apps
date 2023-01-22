622ce46c43d52c9efafa98feca4e1f1a60dde352
Muhammed Gaygisiz <muhammed.gaygisiz@sbb.ch>
2023-01-22 15:05:49 +0100

    feat: change task-object component to standalone component

--- libs/kosaml/conceptual-design/task-object/feature/src/lib/task-object.module.ts
+++ libs/kosaml/conceptual-design/task-object/feature/src/lib/task-object.module.ts
@@ -3,17 +3,9 @@ import { CommonModule } from '@angular/common';
 import { TaskObjectRoutingModule } from './task-object-routing.module';
 import { TaskObjectPageComponent } from './containers/task-object-page.component';
 import { KosamlPageFeatureModule } from '@travellers-apps/kosaml/page/feature';
-import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
-import { TaskObjectComponent } from './components/task-object/task-object.component';
 
 @NgModule({
-  declarations: [TaskObjectPageComponent, TaskObjectComponent],
-  imports: [
-    CommonModule,
-    TaskObjectRoutingModule,
-    KosamlPageFeatureModule,
-    MatTableModule,
-  ],
-  exports: [TaskObjectComponent],
+  declarations: [TaskObjectPageComponent],
+  imports: [CommonModule, TaskObjectRoutingModule, KosamlPageFeatureModule],
 })
 export class TaskObjectModule {}

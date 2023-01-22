976d0db23110ba162909b798a7ad15466beac2a0
Muhammed Gaygisiz <muhammed.gaygisiz@sbb.ch>
2023-01-22 15:07:06 +0100

    feat: add missing import to task-object module

--- libs/kosaml/conceptual-design/task-object/feature/src/lib/task-object.module.ts
+++ libs/kosaml/conceptual-design/task-object/feature/src/lib/task-object.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { TaskObjectRoutingModule } from './task-object-routing.module';
 import { TaskObjectPageComponent } from './containers/task-object-page.component';
 import { KosamlPageFeatureModule } from '@travellers-apps/kosaml/page/feature';
+import { TaskObjectComponent } from './components/task-object/task-object.component';
 
 @NgModule({
   declarations: [TaskObjectPageComponent],
-  imports: [CommonModule, TaskObjectRoutingModule, KosamlPageFeatureModule],
+  imports: [
+    CommonModule,
+    TaskObjectRoutingModule,
+    KosamlPageFeatureModule,
+    TaskObjectComponent,
+  ],
 })
 export class TaskObjectModule {}

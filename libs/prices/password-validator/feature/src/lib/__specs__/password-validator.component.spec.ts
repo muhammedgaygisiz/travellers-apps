707dd4ed0c61b5a60582e1d8ce11afe4d2fc95c5
Muhammed Gaygisiz <muhammed.gaygisiz@sbb.ch>
2023-01-29 11:24:15 +0100

    test(prices): fix test

--- libs/prices/password-validator/feature/src/lib/__specs__/password-validator.component.spec.ts
+++ libs/prices/password-validator/feature/src/lib/__specs__/password-validator.component.spec.ts
@@ -1,17 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PasswordValidatorComponent } from '../component/password-validator.component';
-import { PasswordValidatorModule } from '../password-validator.module';
 
 describe('PasswordValidatorComponent', () => {
   let component: PasswordValidatorComponent;
   let fixture: ComponentFixture<PasswordValidatorComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PasswordValidatorModule],
-    }).compileComponents();
-  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PasswordValidatorComponent);

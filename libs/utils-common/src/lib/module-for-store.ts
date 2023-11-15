d6e3e361df60f0ef5a6706882adeafd679420e48
e529454 <muhammed.gaygisiz@sbb.ch>
2023-11-15 09:50:53 +0100

    chore: fix linting errors

--- libs/utils-common/src/lib/module-for-store.ts
+++
@@ -1,4 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { StoreRootModule } from '@ngrx/store/src/store_module';
-
-export type ModuleForStore = ModuleWithProviders<StoreRootModule> | any[];

a71a516c7df8b9d2c1074d3b3f452a9ed25fdb39
e529454 <muhammed.gaygisiz@sbb.ch>
2023-12-14 11:37:16 +0100

    feat: add empty home page

---
+++ libs/prices/home/feature/src/lib/__specs__/data.ts
@@ -0,0 +1,29 @@
+export const PRICES = [
+  {
+    id: '1',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/6/69/Aroma_%28apple%29.jpg',
+    name: 'Apples',
+    price: 0.2,
+    location: 'Marrakech',
+  },
+  {
+    id: '2',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Assortment_of_pears.jpg',
+    name: 'Bears',
+    price: 0.2,
+    location: 'Marrakech',
+  },
+  {
+    id: '3',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Autumn_Red_peaches.jpg',
+    name: 'Peaches',
+    price: 0.2,
+    location: 'Marrakech',
+  },
+];
+const MARRAKECH = {
+  type: 'location',
+  value: 'Marrakech',
+};
+
+export const FILTERS = [MARRAKECH];

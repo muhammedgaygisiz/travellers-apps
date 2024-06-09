e419cf24f6768272a1ff416651549582c60d80d4
e529454 <muhammed.gaygisiz@sbb.ch>
2024-06-09 15:42:44 +0200

    refactor(kosaml): fix stories for kosaml

--- libs/kosaml/conceptual-design/task-object/feature/src/lib/components/task-object/__specs__/task-object.stories.ts
+++ libs/kosaml/conceptual-design/task-object/feature/src/lib/components/task-object/__specs__/task-object.stories.ts
@@ -1,30 +1,32 @@
 import { TaskObjectComponent } from '../task-object.component';
-import { Meta, Story } from '@storybook/angular';
+import { Meta, StoryObj } from '@storybook/angular';
 
 export default {
   title: 'Conceptual Design/Task Object',
   component: TaskObjectComponent,
+  render: (args) => ({
+    props: { ...args },
+  }),
 } as Meta<TaskObjectComponent>;
 
-const Template: Story<TaskObjectComponent> = (args: TaskObjectComponent) => ({
-  props: args,
-});
+type Story = StoryObj<TaskObjectComponent>;
 
-export const TaskObject = Template.bind({});
-TaskObject.args = {
-  dataSource: [
-    {
-      id: '1',
-      taskObject: 'CD-ROM',
-      attributes: [
-        'Keywords',
-        'Title',
-        'Author',
-        'Year',
-        'Platform',
-        'Owned by (academic, researcher, or research student)',
-      ],
-      actions: ['View', 'Add', 'Print', 'Delete', 'Save', 'Reserve', 'Edit'],
-    },
-  ],
+export const TaskObject: Story = {
+  args: {
+    dataSource: [
+      {
+        id: '1',
+        taskObject: 'CD-ROM',
+        attributes: [
+          'Keywords',
+          'Title',
+          'Author',
+          'Year',
+          'Platform',
+          'Owned by (academic, researcher, or research student)',
+        ],
+        actions: ['View', 'Add', 'Print', 'Delete', 'Save', 'Reserve', 'Edit'],
+      },
+    ],
+  },
 };

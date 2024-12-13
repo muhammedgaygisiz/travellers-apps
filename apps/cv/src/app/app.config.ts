302151a9749acf936083f4b0b18830b5c8e10be8
e529454 <muhammed.gaygisiz@sbb.ch>
2024-12-13 13:20:28 +0100

    chore: cleanup

--- apps/cv/src/app/app.config.ts
+++ apps/cv/src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes),
   ],
 };

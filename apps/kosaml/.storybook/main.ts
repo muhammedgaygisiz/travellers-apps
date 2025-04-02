48d93d4055031af44f10884b55478f5a83ff083b
Muhammed Gaygisiz <54054345+muhammedgaygisiz@users.noreply.github.com>
2025-04-03 01:28:31 +0200

    209 use nx loki (#210)
    * chore: add nx-loki plugin
    
    * chore: setup debugging in vscode
    
    * chore: remove nx-loki
    
    * chore: remove nx-loki plugin from nx.json
    
    * chore: update storybook configuration
    
    * chore: execute storybook automigrations
    
    * chore: cleanup
    
    * chore: add missing import for polyfills
    
    * chore: remove unknown option in main.ts
    
    * chore: fix storybook for prices
    
    * chore: add storybook plugin to nx.json
    
    * chore: remove overrides in package.json
    
    * chore: update nx
    
    * chore: resolve dependency conflicts
    
    * chore: move to nx-loki
    
    * chore: npm i
    
    * chore: disable daemon in pipeline
    
    * ci: verbose logging and nx daemon reset
    
    ---------
    
    Co-authored-by: e529454 <muhammed.gaygisiz@sbb.ch>

---
+++ apps/kosaml/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../../../libs/kosaml/**/__specs__/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@chromatic-com/storybook'],
+
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+
+  docs: {},
+};
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
---
+++ apps/prices/.storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: [
+    '../../../libs/prices/**/__specs__/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+    '../../../libs/common/**/__specs__/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+    '../../../libs/prices/**/__specs__/**/*.mdx',
+  ],
+
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-mdx-gfm',
+    '@chromatic-com/storybook',
+  ],
+  staticDirs: [
+    {
+      from: '../src/assets/i18n',
+      to: '/assets/i18n',
+    },
+  ],
+
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+
+  docs: {},
+};
+
+export default config;

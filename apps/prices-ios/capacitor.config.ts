da1fb8714498453a7e79e0bbd1b26248ec772f22
Muhammed Gaygisiz <54054345+muhammedgaygisiz@users.noreply.github.com>
2025-04-04 10:42:42 +0200

    chore: move cap out of prices and initiale impl of loki reference update pipeline

--- apps/prices-ios/capacitor.config.ts
+++ apps/prices-ios/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.travellersapps.prices',
   appName: 'prices',
-  webDir: 'dist/apps/prices',
+  webDir: '../../dist/apps/prices',
   bundledWebRuntime: false,
 };
 

933841327fb1b52fc3225a97f93a28ce04ba150f
Muhammed <muhammed.gaygisiz@gmail.com>
2025-04-04 18:21:47 +0200

    chore: setup capacitor for finances

---
+++ apps/finances-ios/capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.travellersapps.finances',
+  appName: 'Travellers Finances',
+  webDir: '../../dist/apps/finances/browser',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;

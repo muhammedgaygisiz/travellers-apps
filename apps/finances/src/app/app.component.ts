5fbe354d659c5176cb11a3a85eb29e07c3d1d287
Muhammed <muhammed.gaygisiz@gmail.com>
2025-04-06 23:34:30 +0200

    chore: revert duplicating addNecessaryIcons

--- apps/finances/src/app/app.component.ts
+++ apps/finances/src/app/app.component.ts
@@ -1,43 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import {
-  add,
-  arrowBackOutline,
-  checkmarkOutline,
-  close,
-  closeOutline,
-  filterOutline,
-  languageOutline,
-  locationOutline,
-  logInOutline,
-  logoApple,
-  logoFacebook,
-  logoGoogle,
-  logOutOutline,
-  menuOutline,
-  pricetagOutline,
-} from 'ionicons/icons';
-
-export const addNecessaryIcons = () => {
-  addIcons({
-    pricetagOutline,
-    filterOutline,
-    logInOutline,
-    logOutOutline,
-    add,
-    close,
-    locationOutline,
-    arrowBackOutline,
-    logoGoogle,
-    logoApple,
-    logoFacebook,
-    closeOutline,
-    checkmarkOutline,
-    menuOutline,
-    languageOutline,
-  });
-};
+import { addNecessaryIcons } from '@travellers-apps/utils-common';
 
 @Component({
   selector: 'finances-root',

ef55a7f7d1d89ae535c0af676c3430d3f43c4930
Muhammed <muhammed.gaygisiz@gmail.com>
2025-04-06 15:03:07 +0200

    chore: adjust test config for add-bank

--- libs/finances/pages/add-bank/src/lib/component/add-bank.component.spec.ts
+++ libs/finances/pages/add-bank/src/lib/component/add-bank.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddBankComponent } from './add-bank.component';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { getIonicConfig } from '@travellers-apps/utils-common';
 
 describe('AddBankComponent', () => {
   let component: AddBankComponent;
   let fixture: ComponentFixture<AddBankComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AddBankComponent],
-    }).compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideIonicAngular(getIonicConfig())],
+    });
 
     fixture = TestBed.createComponent(AddBankComponent);
     component = fixture.componentInstance;

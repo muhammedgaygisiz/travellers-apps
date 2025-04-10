9bf1a37488fbfde9b35331f02f7e14da2728837d
Muhammed <muhammed.gaygisiz@gmail.com>
2025-04-11 01:05:26 +0200

    test: extend test suite

--- libs/finances/payment/payment/src/lib/components/payment/payment.component.spec.ts
+++ libs/finances/payment/payment/src/lib/components/payment/payment.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PaymentComponent } from './payment.component';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { ComponentRef } from '@angular/core';
 
 describe('AddPaymentComponent', () => {
   let component: PaymentComponent;
   let fixture: ComponentFixture<PaymentComponent>;
+  let compRef: ComponentRef<PaymentComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +15,19 @@ describe('AddPaymentComponent', () => {
 
     fixture = TestBed.createComponent(PaymentComponent);
     component = fixture.componentInstance;
+    compRef = fixture.componentRef;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update the form group when payment input changes', () => {
+    const mockPayment = { amount: 100 };
+    compRef.setInput('payment', mockPayment);
+
+    fixture.detectChanges();
+
+    expect(component.paymentFormGroup.value).toEqual({ amount: 100 });
   });
 });

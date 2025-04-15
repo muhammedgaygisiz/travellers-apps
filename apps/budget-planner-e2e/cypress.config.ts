06a50426477096162693b27b13a890250e5b93a3
Muhammed <muhammed.gaygisiz@gmail.com>
2025-04-15 15:50:12 +0200

    chore: update nx

--- apps/budget-planner-e2e/cypress.config.ts
+++ apps/budget-planner-e2e/cypress.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
       cypressDir: 'src',
     }),
     baseUrl: 'http://localhost:4200',
+    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
+    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
+    injectDocumentDomain: true,
   },
 });
--- apps/finances-e2e/cypress.config.ts
+++ apps/finances-e2e/cypress.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
   e2e: {
     ...nxE2EPreset(__filename, { cypressDir: 'src' }),
     baseUrl: 'http://localhost:4200',
+    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
+    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
+    injectDocumentDomain: true,
   },
 });

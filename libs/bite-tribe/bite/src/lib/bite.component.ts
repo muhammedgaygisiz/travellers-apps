3f2302cdd830ceabcbc6d17165a19421cce39b86
Muhammed <muhammed.gaygisiz@gmail.com>
2025-04-26 10:40:12 +0200

    feat: new bite page

--- libs/bite-tribe/bite/src/lib/bite.component.ts
+++ libs/bite-tribe/bite/src/lib/bite.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageComponent } from 'common/ui/page';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonList,
+} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'lib-bite-tribe-bite',
-  imports: [CommonModule],
+  selector: 'bt-bite',
+  imports: [
+    CommonModule,
+    PageComponent,
+    IonCard,
+    IonIcon,
+    IonCardContent,
+    IonList,
+    IonItem,
+    IonInput,
+    IonButton,
+    RouterLink,
+    IonContent,
+  ],
   templateUrl: './bite.component.html',
-  styleUrl: './bite.component.css',
+  styleUrl: './bite.component.scss',
 })
 export class BiteTribeBiteComponent {}
--- libs/common/utils/src/lib/add-necessary-icons.ts
+++ libs/common/utils/src/lib/add-necessary-icons.ts
@@ -15,6 +15,7 @@ import {
   checkmarkOutline,
   menuOutline,
   languageOutline,
+  imageOutline,
 } from 'ionicons/icons';
 
 export const addNecessaryIcons = () => {
@@ -34,5 +35,6 @@ export const addNecessaryIcons = () => {
     checkmarkOutline,
     menuOutline,
     languageOutline,
+    imageOutline,
   });
 };

1cefb5e689de99c43571d548ec79fab745771a8f
Muhammed <muhammed.gaygisiz@gmail.com>
2025-05-04 14:23:31 +0200

    test: extend test suite

---
+++ libs/bite-tribe/details/page/src/lib/integration/__specs__/details.service.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed } from '@angular/core/testing';
+import { DetailsService } from '../details.service';
+import { DetailsDataAccessService } from 'bite-tribe/details-data-access';
+import { signal } from '@angular/core';
+
+describe('DetailsService', () => {
+  let service: DetailsService;
+  let mockDataAccessService: jest.Mocked<DetailsDataAccessService>;
+
+  beforeEach(() => {
+    mockDataAccessService = {
+      bite: signal('mockBiteData'),
+    } as any;
+
+    TestBed.configureTestingModule({
+      providers: [
+        DetailsService,
+        { provide: DetailsDataAccessService, useValue: mockDataAccessService },
+      ],
+    });
+
+    service = TestBed.inject(DetailsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should have bite data from DetailsDataAccessService', () => {
+    expect(service.bite()).toBe('mockBiteData');
+  });
+});

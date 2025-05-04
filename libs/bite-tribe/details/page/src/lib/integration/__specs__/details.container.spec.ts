ebc80d0d0eafdcf27389f76fff6d845e839cc31f
Muhammed <muhammed.gaygisiz@gmail.com>
2025-05-04 14:18:50 +0200

    test: fix tests

--- libs/bite-tribe/details/page/src/lib/integration/__specs__/details.container.spec.ts
+++ libs/bite-tribe/details/page/src/lib/integration/__specs__/details.container.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DetailsContainer } from './details.container';
+import { DetailsContainer } from '../details.container';
+import { DetailsService } from '../details.service';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { signal } from '@angular/core';
 
 describe('DetailsComponent', () => {
   let component: DetailsContainer;
   let fixture: ComponentFixture<DetailsContainer>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DetailsContainer],
-    }).compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideIonicAngular(),
+        { provide: DetailsService, useValue: { bite: signal(undefined) } },
+      ],
+    });
 
     fixture = TestBed.createComponent(DetailsContainer);
     component = fixture.componentInstance;

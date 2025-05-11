1d47ccb7c0aad11a445a95c2661756ff5d61e50b
Muhammed <muhammed.gaygisiz@gmail.com>
2025-05-11 18:29:57 +0200

    test: extend test suite

--- libs/common/ui/auth/src/lib/integration/login/__specs__/login.service.spec.ts
+++ libs/common/ui/auth/src/lib/integration/login/__specs__/login.service.spec.ts
@@ -116,4 +116,30 @@ describe('LoginService', () => {
       expect(loginWithFacebookSpy).toHaveBeenCalled();
     });
   });
+
+  describe('when store is not available', () => {
+    beforeEach(() => {
+      TestBed.resetTestingModule();
+      TestBed.configureTestingModule({
+        providers: [
+          provideRouter([{ path: 'registration', redirectTo: '' }]),
+          { provide: STORE_SERVICE, useValue: null },
+        ],
+      }).compileComponents();
+
+      service = TestBed.inject(LoginService);
+      navController = TestBed.inject(NavController);
+    });
+
+    it('should handle login without error', () => {
+      const authCreds = { email: 'email', password: 'password' };
+      expect(() => service.login(authCreds)).not.toThrow();
+    });
+
+    it('should handle social logins without error', () => {
+      expect(() => service.loginWithGoogleAccount()).not.toThrow();
+      expect(() => service.loginWithAppleAccount()).not.toThrow();
+      expect(() => service.loginWithFacebookAccount()).not.toThrow();
+    });
+  });
 });

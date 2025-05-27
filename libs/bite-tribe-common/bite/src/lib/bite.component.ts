6276bffdd410fa8c4b32b8856f45491c89e27777
Muhammed <muhammed.gaygisiz@gmail.com>
2025-05-27 16:04:16 +0200

    refactor: extract likes component

--- libs/bite-tribe-common/bite/src/lib/bite.component.ts
+++ libs/bite-tribe-common/bite/src/lib/bite.component.ts
@@ -1,25 +1,21 @@
 import {
+  booleanAttribute,
   ChangeDetectionStrategy,
   Component,
-  computed,
-  inject,
   input,
   output,
 } from '@angular/core';
 import {
-  IonBadge,
   IonCard,
   IonCardContent,
   IonCardHeader,
   IonCardSubtitle,
   IonCardTitle,
-  IonChip,
-  IonLabel,
 } from '@ionic/angular/standalone';
 import { Bite } from 'model';
 import { PopoverController } from '@ionic/angular';
-import { LikeOptionsPopoverMenuComponent } from './like-options-popover-menu/like-options-popover-menu.component';
 import { ToMetricPipe } from 'distance-pipe';
+import { LikesComponent } from './likes/likes.component';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -32,68 +28,19 @@ import { ToMetricPipe } from 'distance-pipe';
     IonCardHeader,
     IonCardSubtitle,
     IonCardTitle,
-    IonChip,
-    IonBadge,
-    IonLabel,
     ToMetricPipe,
+    LikesComponent,
   ],
   providers: [PopoverController],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
 export class BiteComponent {
-  popoverController = inject(PopoverController);
-
   bite = input.required<Bite>();
   userId = input<string>();
+  showEditButton = input(false, { transform: booleanAttribute });
 
   biteClick = output<Bite>();
   restaurantClick = output<Bite>();
-
   likeButtonClick = output<{ likeType: string; biteId: string }>();
-
-  calcClass = computed(() => {
-    const bite = this.bite();
-    const userId = this.userId();
-
-    const foundLike = bite?.likes?.find((like) => like.userId === userId);
-
-    if (foundLike) {
-      return 'liked';
-    }
-
-    return '';
-  });
-
-  getLikeEmojis = computed(() => {
-    const bite = this.bite();
-    if (!bite?.likes) return [];
-
-    const emojiMap: Record<string, string> = {
-      thumbup: '👍',
-      drooling: '🤤',
-      mindblown: '🤯',
-    };
-
-    return [...new Set(bite.likes.map((like) => emojiMap[like.likeType]))];
-  });
-
-  async openLikeOptions($event: MouseEvent) {
-    const popover = await this.popoverController.create({
-      component: LikeOptionsPopoverMenuComponent,
-      event: $event,
-      dismissOnSelect: true,
-      componentProps: {
-        bite: this.bite,
-        userId: this.userId,
-        likeButtonClick: this.likeButtonClick,
-      },
-      cssClass: 'like-options-popover',
-      alignment: 'center',
-      size: 'auto',
-      arrow: true,
-    });
-
-    await popover.present();
-  }
 }

b9b9d5ca8d50fc9ffed7e6d2034aeed16ed3b58d
Muhammed <muhammed.gaygisiz@gmail.com>
2025-06-08 11:47:54 +0200

    refactor: introduce virtual scrolling

--- libs/bite-tribe/home/page/src/lib/components/page/home.component.ts
+++ libs/bite-tribe/home/page/src/lib/components/page/home.component.ts
@@ -2,6 +2,7 @@ import {
   booleanAttribute,
   ChangeDetectionStrategy,
   Component,
+  inject,
   input,
   output,
 } from '@angular/core';
@@ -15,6 +16,9 @@ import {
 } from '@ionic/angular/standalone';
 import { Bite } from 'model';
 import { BiteComponent } from 'bite-tribe-common/bite';
+import { ScrollingModule, ViewportRuler } from '@angular/cdk/scrolling';
+import { CdkAutoSizeVirtualScroll } from '@angular/cdk-experimental/scrolling';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'bt-home',
@@ -28,24 +32,31 @@ import { BiteComponent } from 'bite-tribe-common/bite';
     IonCard,
     IonCardContent,
     IonText,
+    ScrollingModule,
+    CdkAutoSizeVirtualScroll,
+    AsyncPipe,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BiteTribeHomeComponent {
+  viewportRuler = inject(ViewportRuler);
+
   bites = input<any[]>();
   enableBackButton = input<boolean>(false);
   userId = input<string>();
   title = input('Bites');
-
   editableBites = input(false, { transform: booleanAttribute });
 
   readonly logoutClick = output();
   readonly addButtonClick = output();
   readonly gotoSettings = output();
-
   readonly gotoMyBites = output();
   readonly likeButtonClick = output<{ likeType: string; biteId: string }>();
   readonly biteClick = output<Bite>();
   readonly restaurantClick = output<Bite>();
   readonly gotoEdit = output<Bite>();
+
+  trackByBite(index: number, bite: Bite): string {
+    return bite.id;
+  }
 }

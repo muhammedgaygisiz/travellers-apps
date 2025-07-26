f48073eb2b10e925d890c73e7d215155cbd6d9b4
Muhammed Gaygisiz <54054345+muhammedgaygisiz@users.noreply.github.com>
2025-07-26 14:53:39 +0200

    feat(265): star feature for bite (#422)
    * feat: impl star rating component and embed it to bite view
    
    * feat: make rating on detail view read only
    
    * test: fix tests
    
    * test: adjust test
    
    * feat: add rating field to create bite page
    
    * test: fix tests
    
    * test: extend test suite
    
    * feat: add star rating to home screen bites
    
    * refactor: feedback from code review
    
    * chore: increase build numbere
    
    * style: pwa assets
    
    * style: setup pwa assets

---
+++ libs/common/ui/star-rating/src/lib/star-rating.component.ts
@@ -0,0 +1,90 @@
+import {
+  booleanAttribute,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  forwardRef,
+  input,
+  output,
+  signal,
+} from '@angular/core';
+import { IonIcon } from '@ionic/angular/standalone';
+import { IsFilledPipe } from './pipes/is-filled.pipe';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'star-rating',
+  templateUrl: './star-rating.component.html',
+  styleUrl: './star-rating.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [IonIcon, IsFilledPipe],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => StarRatingComponent),
+      multi: true,
+    },
+  ],
+})
+export class StarRatingComponent implements ControlValueAccessor {
+  rating = input<number | undefined>(0);
+
+  max = input(5);
+
+  readOnly = input(false, { transform: booleanAttribute });
+
+  rated = output<number>();
+
+  hoveredIndex = signal(-1);
+
+  getStars = computed(() => {
+    const max = this.max();
+
+    return Array(max)
+      .fill(0)
+      .map((_, i) => i + 1);
+  });
+
+  onRate(rating: number) {
+    if (this.readOnly()) {
+      return;
+    }
+
+    this.rated.emit(rating);
+    this.setValueAndTriggerChange(rating);
+  }
+
+  setValueAndTriggerChange(rating: number) {
+    this.writeValue(rating);
+    this._onChange(rating);
+    this._onTouch();
+  }
+
+  onHover(index: number) {
+    if (!this.readOnly()) this.hoveredIndex.set(index);
+  }
+
+  onLeave() {
+    this.hoveredIndex.set(-1);
+  }
+
+  value = signal<number | null>(null);
+
+  writeValue(obj: any): void {
+    this.value.set(obj);
+  }
+
+  // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-empty-function
+  _onChange: (value: number | null) => void = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  _onTouch: () => void = () => {};
+
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this._onTouch = fn;
+  }
+}

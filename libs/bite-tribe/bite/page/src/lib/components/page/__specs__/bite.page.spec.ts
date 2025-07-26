5fa2b3babc336e63ac7c1136d820640eb57dc463
Muhammed <muhammed.gaygisiz@gmail.com>
2025-07-26 13:49:27 +0200

    test: fix tests

--- libs/bite-tribe/bite/page/src/lib/components/page/__specs__/bite.page.spec.ts
+++ libs/bite-tribe/bite/page/src/lib/components/page/__specs__/bite.page.spec.ts
@@ -103,6 +103,7 @@ describe('BitePage', () => {
       place: 'Test Place',
       tags: 'fish healthy',
       price: 9.99,
+      rating: 0,
       currency: 'EUR',
       restaurantId: '',
       position: {
@@ -164,6 +165,7 @@ describe('BitePage', () => {
         name: 'Test Bite',
         place: 'Test Place',
         price: 10,
+        rating: 0,
         currency: 'USD',
         tags: 'test food',
         position: { latitude: 42, longitude: 24 },

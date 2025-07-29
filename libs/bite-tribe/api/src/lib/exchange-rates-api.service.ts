458d5fe2832aa7ed963efa2ef8a585efc218578d
Muhammed <muhammed.gaygisiz@gmail.com>
2025-07-29 22:42:38 +0200

    feat: implement sorting by price

--- libs/bite-tribe/api/src/lib/exchange-rates-api.service.ts
+++ libs/bite-tribe/api/src/lib/exchange-rates-api.service.ts
@@ -3,9 +3,18 @@ import { FirebaseFirestore } from '@capacitor-firebase/firestore';
 
 export const EXCHANGERATES = 'meta/exchangeRates';
 
+// eslint-disable-next-line no-unused-vars
+const exchangeRates = {};
+
 @Injectable({ providedIn: 'root' })
 export class ExchangeRatesApiService {
   async getExchangeRates() {
+    // TODO: feature in business app to update exchange rates
+    // await FirebaseFirestore.setDocument({
+    //   reference: EXCHANGERATES,
+    //   data: exchangeRates
+    // })
+
     const doc = await FirebaseFirestore.getDocument({
       reference: EXCHANGERATES,
     });
--- libs/bite-tribe/home/page/src/lib/utils/sort-bites-by-price.ts
+++ libs/bite-tribe/home/page/src/lib/utils/sort-bites-by-price.ts
@@ -1,5 +1,19 @@
 import { Bite } from 'model';
 
+const getNormalizedPrice = (
+  bite: Bite,
+  exchangeRates: Record<string, number>
+) => {
+  if (bite.currency && bite.price) {
+    const rate = exchangeRates[bite.currency] || 1; // Default to 1 if no rate found
+    const price = bite.price;
+
+    return price / rate;
+  }
+
+  return 0; // Return 0 if no price or currency is defined
+};
+
 export const sortBitesByPrice = (
   bites: Bite[],
   exchangeRates: Record<string, number>
@@ -9,5 +23,10 @@ export const sortBitesByPrice = (
     exchangeRates
   );
 
-  return bites;
+  return bites.sort((a: Bite, b: Bite) => {
+    return (
+      getNormalizedPrice(a, exchangeRates) -
+      getNormalizedPrice(b, exchangeRates)
+    );
+  });
 };

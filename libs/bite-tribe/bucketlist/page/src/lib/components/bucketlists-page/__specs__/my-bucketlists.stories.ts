f5e65275e7f94579862027b724d7165f30a679c4
Muhammed <muhammed.gaygisiz@gmail.com>
2025-11-16 14:09:38 +0100

    docs: all pages completed in storybook

---
+++ libs/bite-tribe/bucketlist/page/src/lib/components/bucketlists-page/__specs__/my-bucketlists.stories.ts
@@ -0,0 +1,51 @@
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
+import { BucketlistsPage } from '../bucketlists.page';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { addNecessaryIcons, APP_TITLE, getIonicConfig } from 'utils';
+import { Bite, Bucketlist, Like } from 'model';
+
+addNecessaryIcons();
+
+export default {
+  title: 'Pages/My Bucketlists',
+  component: BucketlistsPage,
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideIonicAngular(getIonicConfig()),
+        { provide: APP_TITLE, useValue: 'Bite Tribe' },
+      ],
+    }),
+  ],
+} as Meta<BucketlistsPage>;
+
+type Story = StoryObj<BucketlistsPage>;
+export const Empty: Story = {
+  args: {},
+};
+
+export const withBucketlists: Story = {
+  args: {
+    bucketlists: [
+      {
+        name: 'Turkey',
+      } as Bucketlist,
+      {
+        name: 'Cologne',
+        biteIds: ['1'],
+      } as Bucketlist,
+      {
+        name: 'Kosovo',
+        biteIds: ['1', '2'],
+      } as Bucketlist,
+      {
+        name: 'Malta',
+        biteIds: ['1', '2', '3', '4', '5'],
+      } as Bucketlist,
+      {
+        name: 'London',
+        biteIds: ['1', '2', '3', '4', '5', '6', '7', '8', '8', '10'],
+      } as Bucketlist,
+    ],
+  },
+};
---
+++ libs/bite-tribe/settings/page/src/lib/components/page/__specs__/settings.stories.ts
@@ -0,0 +1,22 @@
+import { addNecessaryIcons, APP_TITLE, getIonicConfig } from 'utils';
+import { PageSettings } from '../settings.component';
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+
+addNecessaryIcons();
+
+export default {
+  title: 'Pages/Settings',
+  component: PageSettings,
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideIonicAngular(getIonicConfig()),
+        { provide: APP_TITLE, useValue: 'Bite Tribe' },
+      ],
+    }),
+  ],
+} as Meta<PageSettings>;
+
+type Story = StoryObj<PageSettings>;
+export const Default: Story = {};

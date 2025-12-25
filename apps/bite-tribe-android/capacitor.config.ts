16e5063c7e8b594af932690238d8e2b22b0bd624
Muhammed <muhammed.gaygisiz@gmail.com>
2025-12-25 17:10:13 +0100

    build: prepare android app

--- apps/bite-tribe-android/capacitor.config.ts
+++ apps/bite-tribe-android/capacitor.config.ts
@@ -4,9 +4,17 @@ const config: CapacitorConfig = {
   appId: 'com.bitetribe.app',
   appName: 'BiteTribe',
   webDir: '../../dist/apps/bite-tribe',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
+  },
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com', 'apple.com'],
+    },
+    SplashScreen: {
+      launchAutoHide: false,
+    },
   },
 };
 

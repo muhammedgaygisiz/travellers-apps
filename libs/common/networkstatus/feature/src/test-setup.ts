41fe9e60c79d90052dff8c9df299e149652d7739
Muhammed <muhammed.gaygisiz@gmail.com>
2026-01-02 18:47:42 +0100

    chore: migrate networkstatus-service to vitest

--- libs/common/networkstatus/feature/src/test-setup.ts
+++ libs/common/networkstatus/feature/src/test-setup.ts
@@ -1,3 +1,13 @@
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+import '@angular/compiler';
+import '@analogjs/vitest-angular/setup-zone';
 
-setupZoneTestEnv();
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
+import { getTestBed } from '@angular/core/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting(),
+);

918d7ac7f8cb833a489ecd7c034c0fc1384a3945
Muhammed <muhammed.gaygisiz@gmail.com>
2026-01-03 11:08:12 +0100

    chore: migrate localization to vitest

--- libs/common/localization/src/test-setup.ts
+++ libs/common/localization/src/test-setup.ts
@@ -1,10 +1,13 @@
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
-  },
-};
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+import '@angular/compiler';
+import '@analogjs/vitest-angular/setup-zone';
 
-setupZoneTestEnv();
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
+import { getTestBed } from '@angular/core/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting(),
+);

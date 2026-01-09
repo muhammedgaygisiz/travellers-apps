2921461e433fec5b292634eabdd3b0de687bbb35
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-01-09 14:36:13 +0100

    test: add test for create-bite function

---
+++ libs/bite-tribe/api/src/lib/bite-api/utils/__specs__/create-bite.spec.ts
@@ -0,0 +1,51 @@
+import { createBite } from '../create-bite';
+import { Bite } from 'model';
+import { User } from '@capacitor-firebase/authentication/dist/esm/definitions';
+import { geohashForLocation } from 'geofire-common';
+import { FirebaseFirestore } from '@capacitor-firebase/firestore';
+
+jest.mock('geofire-common', () => ({ geohashForLocation: jest.fn() }));
+jest.mock('@capacitor-firebase/firestore', () => ({
+  FirebaseFirestore: {
+    addDocument: jest.fn(),
+  },
+}));
+
+describe('createBite', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    const mockDate = new Date('2024-03-15T12:00:00Z');
+    jest.setSystemTime(mockDate);
+    (geohashForLocation as jest.Mock).mockReturnValue('123');
+    (FirebaseFirestore.addDocument as jest.Mock).mockReturnValue({
+      reference: { id: '7' },
+    });
+  });
+
+  describe('given a bite and user', () => {
+    const bite = {
+      position: { latitude: 10, longitude: 20 },
+    } as Omit<Bite, 'image'>;
+
+    const user = { uid: '789' } as User;
+
+    it('should call addDocument with ', () => {
+      createBite(bite, user);
+
+      expect(geohashForLocation).toHaveBeenCalledWith([10, 20]);
+      expect(FirebaseFirestore.addDocument).toHaveBeenCalledWith({
+        reference: 'bites',
+        data: {
+          createdAt: '2024-03-15T12:00:00.000Z',
+          createdAtTimestamp: 1710504000000,
+          geohash: '123',
+          position: {
+            latitude: 10,
+            longitude: 20,
+          },
+          userId: '789',
+        },
+      });
+    });
+  });
+});

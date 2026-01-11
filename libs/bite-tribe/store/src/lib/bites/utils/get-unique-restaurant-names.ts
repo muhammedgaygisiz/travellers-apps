29d212f16353a7aef35d6ca7524ea4345aba97d9
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-01-11 14:39:16 +0100

    feat: take longest restaurant name in fuzzy comparison

--- libs/bite-tribe/store/src/lib/bites/utils/get-unique-restaurant-names.ts
+++ libs/bite-tribe/store/src/lib/bites/utils/get-unique-restaurant-names.ts
@@ -1,20 +1,45 @@
 import { containsFuzzyEqualRestaurantName } from './contains-fuzzy-equal-restaurant-name';
 import { Bite } from 'model';
+import { isSimilar } from './is-similar';
+
+const getLongestFuzzyEqualRestaurantName = (
+  place: string,
+  places: string[],
+): string => {
+  const candidates = new Set<string>();
+
+  places.forEach((p) => {
+    const name1 = p.length > place.length ? place : p;
+    const name2 = p.length > place.length ? p : place;
+
+    if (isSimilar(name1, name2)) {
+      candidates.add(p);
+    }
+  });
+
+  const candidatesArray = Array.from(candidates);
+
+  return candidatesArray.reduce(function (a, b) {
+    return a.length > b.length ? a : b;
+  });
+};
 
 export const getUniqueRestaurantNames = (bites: Bite[]): Set<string> => {
   const restaurantNames = new Set<string>();
 
-  bites.forEach((bite) => {
-    const restaurantName = bite.place && bite.place.trim();
+  const places = bites
+    .map((bite) => bite?.place?.trim())
+    .filter((place) => !!place);
 
+  places.forEach((place) => {
     const listContainsFuzzyEqualRestaurantName =
-      containsFuzzyEqualRestaurantName(
-        restaurantName,
-        Array.from(restaurantNames),
-      );
+      containsFuzzyEqualRestaurantName(place, Array.from(restaurantNames));
 
-    if (restaurantName && !listContainsFuzzyEqualRestaurantName) {
-      restaurantNames.add(restaurantName);
+    if (place && !listContainsFuzzyEqualRestaurantName) {
+      const longestFuzzyEqualRestaurantNames =
+        getLongestFuzzyEqualRestaurantName(place, places);
+
+      restaurantNames.add(longestFuzzyEqualRestaurantNames);
     }
   });
 

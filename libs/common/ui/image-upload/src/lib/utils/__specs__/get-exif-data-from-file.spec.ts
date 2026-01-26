290f7a78cc0a39be2f3be56c190630647b2f1a01
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-01-26 13:24:24 +0100

    test: extend test suite

--- libs/common/ui/image-upload/src/lib/utils/__specs__/get-exif-data-from-file.spec.ts
+++ libs/common/ui/image-upload/src/lib/utils/__specs__/get-exif-data-from-file.spec.ts
@@ -12,49 +12,53 @@ describe('getExifDataFromFile', () => {
     jest.clearAllMocks();
   });
 
-  it('should extract GPS position from valid EXIF data', async () => {
-    (EXIFR.parse as jest.Mock).mockImplementation(() => ({
-      latitude: 40.5,
-      longitude: 74,
-    }));
+  describe('given a file with gps meta data', () => {
+    it('should extract GPS position from valid EXIF data', async () => {
+      (EXIFR.parse as jest.Mock).mockImplementation(() => ({
+        latitude: 40.5,
+        longitude: 74,
+      }));
 
-    const result = await getExifDataFromFile(mockFile);
-    expect(result).toEqual({
-      latitude: 40.5,
-      longitude: 74,
-    });
-    expect(EXIFR.parse).toHaveBeenCalled();
-  });
-
-  it('should return undefined if EXIF data is missing', async () => {
-    (EXIFR.parse as jest.Mock).mockImplementation(() => ({
-      latitude: 0,
-      longitude: 0,
-    }));
-
-    const result = await getExifDataFromFile(mockFile);
-    expect(result).toBeUndefined();
-    expect(EXIFR.parse).toHaveBeenCalled();
-  });
-
-  it('should reject if an error is thrown', async () => {
-    const error = new Error('EXIF error');
-    (EXIFR.parse as jest.Mock).mockImplementation(() => {
-      throw error;
+      const result = await getExifDataFromFile(mockFile);
+      expect(result).toEqual({
+        latitude: 40.5,
+        longitude: 74,
+      });
+      expect(EXIFR.parse).toHaveBeenCalled();
     });
 
-    const result = await getExifDataFromFile(mockFile);
-    expect(result).toEqual(undefined);
-    expect(EXIFR.parse).toHaveBeenCalled();
+    it('should return undefined if EXIF data is missing', async () => {
+      (EXIFR.parse as jest.Mock).mockImplementation(() => ({
+        latitude: 0,
+        longitude: 0,
+      }));
+
+      const result = await getExifDataFromFile(mockFile);
+      expect(result).toBeUndefined();
+      expect(EXIFR.parse).toHaveBeenCalled();
+    });
   });
 
-  it('should return fallback position if getTag throws inside getData', async () => {
-    (EXIFR.parse as jest.Mock).mockImplementation(() => ({
-      latitude: 123,
-      longitude: 456,
-    }));
+  describe('given a file with exif data without gps fields', () => {
+    it('should return undefined if GPS data is missing in EXIF', async () => {
+      (EXIFR.parse as jest.Mock).mockImplementation(() => ({}));
 
-    const result = await getExifDataFromFile(mockFile);
-    expect(result).toEqual({ latitude: 123, longitude: 456 });
+      const result = await getExifDataFromFile(mockFile);
+      expect(result).toBeUndefined();
+      expect(EXIFR.parse).toHaveBeenCalled();
+    });
+  });
+
+  describe('given an error', () => {
+    it('should reject if an error is thrown', async () => {
+      const error = new Error('EXIF error');
+      (EXIFR.parse as jest.Mock).mockImplementation(() => {
+        throw error;
+      });
+
+      const result = await getExifDataFromFile(mockFile);
+      expect(result).toEqual(undefined);
+      expect(EXIFR.parse).toHaveBeenCalled();
+    });
   });
 });

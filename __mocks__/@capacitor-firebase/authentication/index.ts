ddda7787a59ba83f80ce72a4a6f6c0355ee9cedf
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-02-19 14:31:01 +0100

    test: adjust tests

---
+++ __mocks__/@capacitor-firebase/authentication/index.ts
@@ -0,0 +1,9 @@
+export const FirebaseAuthentication = {
+  getCurrentUser: jest.fn(),
+  addListener: jest.fn(),
+  signInWithEmailAndPassword: jest.fn(),
+  signOut: jest.fn(),
+  signInWithGoogle: jest.fn(),
+  signInWithApple: jest.fn(),
+  createUserWithEmailAndPassword: jest.fn(),
+};

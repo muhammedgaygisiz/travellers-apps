26c0c402a35c08568cd531fd108d1a0943325d53
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-03-23 06:27:05 +0100

    feat: basic setup for organisation dashboard

--- libs/bite-tribe-business/organisation-dashboard/page/src/lib/components/page/organisation-dashboard.ts
+++ libs/bite-tribe-business/organisation-dashboard/page/src/lib/components/page/organisation-dashboard.ts
@@ -1,11 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PageComponent } from 'common/ui/page';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+} from '@ionic/angular/standalone';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'organisation-dashboard',
   templateUrl: 'organisation-dashboard.html',
-  imports: [PageComponent],
+  imports: [
+    PageComponent,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonList,
+    IonItem,
+    IonLabel,
+  ],
   styleUrl: 'organisation-dashboard.scss',
 })
-export class OrganisationDashboard {}
+export class OrganisationDashboard {
+  protected selectEmployee(): void {
+    //TODO: implement employee selection logic
+  }
+}

2a7dc5a9230b89dd8ebb05044f81f86399f76c7c
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-04-19 23:17:58 +0200

    test: adjust tests

--- libs/bite-tribe/details/page/src/lib/integration/__specs__/details.container.spec.ts
+++ libs/bite-tribe/details/page/src/lib/integration/__specs__/details.container.spec.ts
@@ -51,6 +51,7 @@ describe(DetailsContainer.name, () => {
             userId: signal(undefined),
             biteCreator: {
               value: signal(undefined),
+              error: signal(undefined),
               reload: jest.fn(),
             },
             // eslint-disable-next-line @typescript-eslint/no-empty-function

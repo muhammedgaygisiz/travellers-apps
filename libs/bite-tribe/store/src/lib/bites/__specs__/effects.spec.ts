53779baaf1b940872530ba6a690c6b0bb9bf9100
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-04-22 20:45:49 +0200

    test: adjust tests

--- libs/bite-tribe/store/src/lib/bites/__specs__/effects.spec.ts
+++ libs/bite-tribe/store/src/lib/bites/__specs__/effects.spec.ts
@@ -537,7 +537,7 @@ describe(BiteEffects.name, () => {
             a: fromAuth.AuthActions.loginSucceeded(),
           });
 
-          const expected = '400ms a';
+          const expected = '200ms (a|)';
           const output = {
             a: BiteActions.loadedLatestFromAPI({ bites: [] }),
           };

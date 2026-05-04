0bf234689619eb3aa583813d642a28cf552994fa
u230030 <17550954+samlanzz@users.noreply.github.com>
2026-05-04 17:43:54 +0200

    feat: 732 style menu-item

--- libs/bite-tribe-business/edit-menu/page/src/lib/components/business-menu-item/__specs__/business-menu-item.component.spec.ts
+++ libs/bite-tribe-business/edit-menu/page/src/lib/components/business-menu-item/__specs__/business-menu-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ComponentRef } from '@angular/core';
 import { BusinessMenuItemComponent } from '../business-menu-item.component';
+import { MenuItem } from 'model';
 import SpyInstance = jest.SpyInstance;
 
 describe('BusinessMenuItemComponent', () => {
@@ -73,6 +74,67 @@ describe('BusinessMenuItemComponent', () => {
       component.onAddVariant(newVariant);
 
       expect(component.presentAddVariant()).toBe(false);
+      expect(emitSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('onItemChange', () => {
+    it('should set form name, description and price from item when form title is empty', () => {
+      component.itemForm.name().value.set('');
+      component.itemForm.description().value.set('');
+      component.itemForm.price().value.set(10);
+      const item: MenuItem = { name: 'Test', description: 'Desc', price: 100 };
+      componentRef.setInput('item', item);
+      componentRef.changeDetectorRef.detectChanges();
+      expect(component.itemForm.name().value()).toBe('Test');
+      expect(component.itemForm.description().value()).toBe('Desc');
+      expect(component.itemForm.price().value()).toBe(100);
+    });
+
+    it('should not set form values if form title has value', () => {
+      component.itemForm.name().value.set('Existing');
+      component.itemForm.description().value.set('Existing');
+      component.itemForm.price().value.set('Existing');
+      const item: MenuItem = { name: 'Test', description: 'Desc', price: 100 };
+      componentRef.setInput('item', item);
+      componentRef.changeDetectorRef.detectChanges();
+      expect(component.itemForm.name().value()).toBe('Existing');
+      expect(component.itemForm.description().value()).toBe('Existing');
+      expect(component.itemForm.price().value()).toBe('Existing');
+    });
+  });
+
+  describe('onItemChange', () => {
+    let emitSpy: SpyInstance;
+
+    beforeEach(() => {
+      emitSpy = jest.spyOn(component.itemChanged, 'emit');
+    });
+
+    it('should emit itemChanged when changes occur', () => {
+      const item: MenuItem = { name: 'Name', description: 'Desc', price: 10 };
+      componentRef.setInput('item', item);
+      component.itemForm.name().value.set('New Name');
+      component.itemForm.description().value.set('New Description');
+      component.itemForm.price().value.set(100);
+      componentRef.changeDetectorRef.detectChanges();
+
+      expect(emitSpy).toHaveBeenCalledWith({
+        ...item,
+        name: 'New Name',
+        description: 'New Description',
+        price: 100,
+      });
+    });
+
+    it('should not emit itemChanged if fields are unchanged', () => {
+      const item: MenuItem = { name: 'Name', description: 'Desc', price: 10 };
+      componentRef.setInput('item', item);
+      component.itemForm.name().value.set('Name');
+      component.itemForm.description().value.set('Desc');
+      component.itemForm.price().value.set(10);
+      componentRef.changeDetectorRef.detectChanges();
+
       expect(emitSpy).not.toHaveBeenCalled();
     });
   });

455d89af98a2296adc909699dd6124617a021cab
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-06-20 19:57:24 +0200

    fix: empty states in profile page

--- libs/bite-tribe/profile/page/src/lib/components/profile-page/__specs__/profile.stories.ts
+++ libs/bite-tribe/profile/page/src/lib/components/profile-page/__specs__/profile.stories.ts
@@ -2,7 +2,7 @@ import { addNecessaryIcons, APP_TITLE, getIonicConfig } from 'utils';
 import { ProfileComponent } from '../profile.component';
 import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
 import { provideIonicAngular } from '@ionic/angular/standalone';
-import { Bite, Like, PublicUser } from 'model';
+import { Bite, BiteTrail, Like, ProfileMetaData, PublicUser } from 'model';
 
 addNecessaryIcons();
 
@@ -20,50 +20,146 @@ export default {
 } as Meta<ProfileComponent>;
 
 type Story = StoryObj<ProfileComponent>;
+
+const currentUserId = 'current-user';
+
+const profileMetadata: ProfileMetaData = {
+  followers: 12,
+  following: 7,
+  isFollowedByMe: false,
+};
+
+const noProfileMetadata: ProfileMetaData = {
+  followers: 0,
+  following: 0,
+  isFollowedByMe: false,
+};
+
+const baseUser: PublicUser = {
+  userId: 'profile-owner',
+  about: "It's me, Mario!",
+  city: 'Berne',
+  displayName: 'Mo',
+  fullName: 'Muhammed Gaygisiz',
+  email: 'mo@example.com',
+  photoUrl: '',
+  public: true,
+  subscriptionTier: 1,
+};
+
+const bite: Bite = {
+  id: 'bite1',
+  name: 'Botanic Breeze',
+  imagePath:
+    'https://firebasestorage.googleapis.com/v0/b/bite-tribe.firebasestorage.app/o/images%2Fbites%2F5RaqIaqErWatltveDVAf%2Fd37622f5-1423-43ea-a16e-f64d71b8b08e.jpg?alt=media&token=8f22c176-6680-424e-97fa-09864cfe30a2',
+  place: 'Einstein au Jardin',
+  distance: '0.6',
+  rating: 3,
+  likes: [{ likeType: 'thumbup' } as Like],
+} as Bite;
+
+const biteTrail: BiteTrail = {
+  id: 'trail1',
+  ownerId: 'organisation-owner',
+  name: 'Bern Brunch Walk',
+  biteIds: ['bite1', 'bite2', 'bite3'],
+  soldCount: 24,
+  imagePath:
+    'https://images.unsplash.com/photo-1551218808-94e220e084d2?w=1200&auto=format&fit=crop',
+  image: '',
+  ownerImagePath: '',
+  ownerName: 'Bite Tribe',
+  location: 'Bern',
+  description: 'A compact morning route through three easygoing brunch spots.',
+  price: 12,
+  currency: 'CHF',
+};
+
 export const Default: Story = {
   args: {
-    user: {
-      userId: '1',
-      about: "It's me, Mario!",
-      city: 'Berne',
-      displayName: 'Mo',
-      photoUrl: '',
-    } as PublicUser,
-    bites: [
-      {
-        id: 'bite1',
-        name: 'Botanic Breeze',
-        imagePath:
-          'https://firebasestorage.googleapis.com/v0/b/bite-tribe.firebasestorage.app/o/images%2Fbites%2F5RaqIaqErWatltveDVAf%2Fd37622f5-1423-43ea-a16e-f64d71b8b08e.jpg?alt=media&token=8f22c176-6680-424e-97fa-09864cfe30a2',
-        place: 'Einstein au Jardin',
-        distance: '0.6',
-        rating: 3,
-        likes: [{ likeType: 'thumbup' } as Like],
-      } as Bite,
-    ],
+    user: baseUser,
+    userId: currentUserId,
+    profileMetadata,
+    bites: [bite],
   },
 };
 
-export const withoutBites: Story = {
+export const OwnProfile: Story = {
   args: {
     user: {
-      about: "It's me, Mario!",
-      city: 'Berne',
-      displayName: 'Mo',
-      photoUrl: '',
-    } as PublicUser,
+      ...baseUser,
+      userId: currentUserId,
+    },
+    userId: currentUserId,
+    profileMetadata,
+    bites: [bite],
+  },
+};
+
+export const WithoutBites: Story = {
+  args: {
+    user: baseUser,
+    userId: currentUserId,
+    profileMetadata,
     bites: [],
   },
 };
 
-export const ownProfile: Story = {
+export const NewUserEmptyProfile: Story = {
   args: {
     user: {
-      about: "It's me, Mario!",
-      city: 'Berne',
-      displayName: 'Mo',
-      photoUrl: '',
-    } as PublicUser,
+      ...baseUser,
+      about: '',
+      city: '',
+      displayName: '',
+      fullName: '',
+      subscriptionTier: 1,
+    },
+    userId: currentUserId,
+    profileMetadata: noProfileMetadata,
     bites: [],
+  },
+};
+
+export const NoFollowersOrFollowing: Story = {
+  args: {
+    user: baseUser,
+    userId: currentUserId,
+    profileMetadata: noProfileMetadata,
+    bites: [bite],
+  },
+};
+
+export const OrganisationWithBiteTrails: Story = {
+  args: {
+    user: {
+      ...baseUser,
+      userId: 'organisation-owner',
+      about: 'Curated food trails around Swiss cities.',
+      city: 'Bern',
+      displayName: 'Bite Tribe Guides',
+      fullName: '',
+      isOrganisation: true,
+    },
+    userId: currentUserId,
+    profileMetadata,
+    biteTrails: [biteTrail],
+  },
+};
+
+export const OrganisationWithoutBiteTrails: Story = {
+  args: {
+    user: {
+      ...baseUser,
+      userId: 'organisation-owner',
+      about: '',
+      city: '',
+      displayName: 'New Food Guide',
+      fullName: '',
+      isOrganisation: true,
+    },
+    userId: currentUserId,
+    profileMetadata: noProfileMetadata,
+    biteTrails: [],
   },
 };

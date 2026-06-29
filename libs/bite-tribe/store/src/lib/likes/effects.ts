9b92fab43b5add40e276d73032e6a766ce957ecc
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-06-29 18:40:06 +0200

    refactor: dont load bites on gps refresh

--- libs/bite-tribe/store/src/lib/likes/effects.ts
+++ libs/bite-tribe/store/src/lib/likes/effects.ts
@@ -6,29 +6,13 @@ import {
   removeLike,
   saveLike,
 } from './actions';
-import { filter, from, map, switchMap } from 'rxjs';
+import { from, map, switchMap } from 'rxjs';
 import { BiteTribeApiService } from 'bite-tribe/api';
-import { BiteActions } from '../bites/actions';
 
 @Injectable()
 export class LikeEffects {
   private readonly actions$ = inject(Actions);
   private readonly api = inject(BiteTribeApiService);
-
-  startListener$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(
-        BiteActions.loadedLatestFromAPI,
-        BiteActions.loadedByGPSPositionFromAPI,
-      ),
-      filter(({ bites }) => bites.length > 0),
-      switchMap(({ bites }) =>
-        from(this.api.loadLikesForBites(bites)).pipe(
-          map((likes) => loadedLikesFromApi({ likes })),
-        ),
-      ),
-    );
-  });
 
   saveLikeToBite$ = createEffect(() => {
     return this.actions$.pipe(

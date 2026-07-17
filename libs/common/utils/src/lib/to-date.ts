47a9609f6adcd239c2480f2fb3324ced6a2501b1
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-07-17 23:37:53 +0200

    refactor: fix lint warnings

--- libs/common/utils/src/lib/to-date.ts
+++ libs/common/utils/src/lib/to-date.ts
@@ -1,1 +1,11 @@
-export const toDate = (a: any): Date => (a.toDate ? a.toDate() : new Date(a));
+type FirestoreTimestampLike = { toDate: () => Date };
+
+export type DateLike = Date | string | number | FirestoreTimestampLike;
+
+const isTimestampLike = (a: unknown): a is FirestoreTimestampLike =>
+  typeof a === 'object' &&
+  a !== null &&
+  typeof (a as FirestoreTimestampLike).toDate === 'function';
+
+export const toDate = (a: DateLike): Date =>
+  isTimestampLike(a) ? a.toDate() : new Date(a);

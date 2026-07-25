c70c3ed82cc8c1ca7944785039efe2ee69146e8c
Muhammed Veysel Gaygisiz <muhammed.gaygisiz@gmail.com>
2026-07-25 20:00:48 +0200

    refactor: remove dead code

--- apps/bite-tribe/env-var-plugin.js
+++ apps/bite-tribe/env-var-plugin.js
@@ -8,11 +8,5 @@ const workspaceRoot = join(__dirname, '../..');
 
 module.exports = createEnvVarPlugin({
   workspaceRoot,
-  allowedKeys: [
-    ...FIREBASE_ENV_KEYS,
-    'NX_APP_BITE_TRIBE_AUTH_DOMAIN',
-    // Called directly from the browser by the reverse-geocoding service, so it
-    // has to be inlined. Restrict it by referrer in the Geoapify dashboard.
-    'NX_APP_GEOAPIFY_API_KEY',
-  ],
+  allowedKeys: [...FIREBASE_ENV_KEYS, 'NX_APP_BITE_TRIBE_AUTH_DOMAIN'],
 });
